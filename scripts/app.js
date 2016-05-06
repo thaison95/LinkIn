fac9ec3f0dd8b91be396af750f9814a80a69fcdc
Thái Sơn Nguyễn <Thi Sn Nguyn>
2016-05-06 12:30:38 +0700

    Edit Summary&Experience

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,13 @@
  * Created by Administrator on 25/04/2016.
  */
 var app = angular.module('myApp', []);
+
 app.filter('to_trusted', ['$sce', function($sce){
         return function(text) {
             return $sce.trustAsHtml(text);
         };
     }]);
+	
 app.controller('myCtrl', function($scope, $http) {
     $scope.showmore=false;
     $http({
@@ -17,4 +19,32 @@ app.controller('myCtrl', function($scope, $http) {
     }, function myError(response) {
         $scope.data = response.statusText;
     });
+	
+	$scope.hoverIn = function(){
+        this.hoverEdit = true;
+    };
+
+    $scope.hoverOut = function(){
+        this.hoverEdit = false;
+    };
+});
+
+app.directive("contenteditable", function() {
+  return {
+    require: "ngModel",
+    link: function(scope, element, attrs, ngModel) {
+
+      function read() {
+        ngModel.$setViewValue(element.html());
+      }
+
+      ngModel.$render = function() {
+        element.html(ngModel.$viewValue || "");
+      };
+
+      element.bind("blur keyup change", function() {
+        scope.$apply(read);
+      });
+    }
+  };
 });
